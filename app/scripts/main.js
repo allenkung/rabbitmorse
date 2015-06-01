aed1378a5910dcf93968cb5023d40b576469ed11
AllenKung <mrgongxue@gmail.com>
2015-06-01 23:05:24 +0800

    add rabbit morse to english function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,55 +4,106 @@
 
   console.log('---------> Rabbit Morse, ' + Date());
 
-  var rabbitMorse = {
-    a : '乎呼',
-    b : '呼乎乎乎',
-    c : '呼乎呼乎',
-    d : '呼乎乎',
-    e : '乎',
-    f : '乎乎呼乎',
-    g : '呼呼乎',
-    h : '乎乎乎乎',
-    i : '乎乎',
-    j : '乎呼呼呼',
-    k : '呼乎呼',
-    l : '乎呼乎乎',
-    m : '呼呼',
-    n : '呼乎',
-    o : '呼呼呼',
-    p : '乎呼呼乎',
-    q : '呼呼乎呼',
-    r : '乎呼乎',
-    s : '乎乎乎',
-    t : '呼',
-    u : '乎乎呼',
-    v : '乎乎乎呼',
-    w : '乎呼呼',
-    x : '呼乎乎呼',
-    y : '呼乎呼呼',
-    z : '呼呼乎乎'
-  };
+  /* Define Rabbit emoji; */
+  var dots = '🐰';
+  var dashes = '🍌';
+  var space = '\ \ ';
 
-  // console.log(rabbitMorse.a);
+  var rabbitMorse = {
+    a : dots + dashes,
+    b : dashes + dots + dots + dots,
+    c : dashes + dots + dashes + dots,
+    d : dashes + dots + dots,
+    e : dots,
+    f : dots + dots + dashes + dots,
+    g : dashes + dashes + dots,
+    h : dots + dots + dots + dots,
+    i : dots + dots,
+    j : dots + dashes + dashes + dashes,
+    k : dashes + dots + dashes,
+    l : dots + dashes + dots + dots,
+    m : dashes + dashes,
+    n : dashes + dots,
+    o : dashes + dashes + dashes,
+    p : dots + dashes + dashes + dots,
+    q : dashes + dashes + dots + dashes,
+    r : dots + dashes + dots,
+    s : dots + dots + dots,
+    t : dashes,
+    u : dots + dots + dashes,
+    v : dots + dots + dots + dashes,
+    w : dots + dashes + dashes,
+    x : dashes + dots + dots + dashes,
+    y : dashes + dots + dashes + dashes,
+    z : dashes + dashes + dots + dots
+  };
 
   $(document).ready(function(){
 
-    $('#r2e').click(function(){
-      var text = $('textarea').val();
-      console.log(text);
-      text = text.split('');
-      console.log(text);
+    $('#e2r').click(function(){
+      if ( $('textarea').val() === '' ) {
+        $('textarea').attr('placeholder','请输入英语 Please input English');
+      } else {
+        console.log('---> e2r');
+        var text = $('textarea').val();
+        console.log(text);
+        text.toLowerCase();
+        console.log(text); // 大写转小写
+        text = text.split('');
+        console.log(text);
 
-      $.each(text,function(key, value){
-        console.log(key + ':' + value);
-        text[key] = rabbitMorse[value];
-      });
+        $.each(text,function(key, value){
+          if (value !== ' ') { // 判断空格
+            console.log(key + ':' + value);
+            text[key] = rabbitMorse[value];
+          } else {
+            text[key] = space;
+          }
 
-      console.log(text);
+        });
+        console.log(text);
+        $('textarea').val(text.join(''));
 
-      $('textarea').val(text.join(' '));
+        $('h1.animated').addClass('bounce');
+        $('h1.animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $('h1.animated').removeClass('bounce');
+        });
+      }
 
     });
+
+    $('#r2e').click(function(){
+      if ( $('textarea').val() === '' ) {
+        $('textarea').attr('placeholder','请输入兔语 呼呼 呼呼呼');
+      } else {
+        console.log('---> r2e');
+        var text = $('textarea').val();
+        console.log(text);
+        text = text.split(' ');
+        console.log(text);
+
+        var x;
+        for (x in text) {
+          var y;
+          for (y in rabbitMorse) {
+            if (text[x]==='') {
+              text[x] = '\ ';
+            } else if (rabbitMorse[y]===text[x]) {
+              text[x] = y;
+            }
+          }
+        }
+        console.log(text);
+
+        $('textarea').val(text.join(''));
+
+        $('h1.animated').addClass('bounce');
+        $('h1.animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $('h1.animated').removeClass('bounce');
+        });
+      }
+    });
+
   });
 
 })();
